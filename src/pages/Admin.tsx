fae8a5e1e865f6ccbba52ebbb37b44f502f1456b
aspariz <rizantaazfa@gmail.com>
2026-07-10 11:53:59 +0700

    afafasggaf

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -4,7 +4,7 @@ import GuruManager from "./GuruManager";
 
 
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://be-mtsn.vercel.app/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://be-mtsn.vercel.app/";
 
 // ─── Types ───────────────────────────────────────────────────
 
